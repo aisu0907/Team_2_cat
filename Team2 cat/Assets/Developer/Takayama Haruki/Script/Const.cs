24564ced8cc97cc6511f0d39ef271f79e550b816
DESKTOP-PK5J6LJ\GA2B <harukigao@gmail.com>
2026-05-11 14:21:24 +0900

    ファイル名ルール Constスクリプトに表記

--- Team2 cat/Assets/Developer/Takayama Haruki/Script/Const.cs
+++ Team2 cat/Assets/Developer/Takayama Haruki/Script/Const.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 namespace Const 
 {
+    //スクリプト名は単語で大文字区切り 例(FpsLimiter)
+    //変数名はすべて小文字の単語をアンダーバー区切り　例(fps_limiter)
+    //関数名は頭文字大文字あとは小文字　例(Fpslimiter)
+
     public static class GameConfig
     {
         public const int TICK_TIME = 60;
