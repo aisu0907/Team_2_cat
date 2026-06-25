90a80825041882b56aa3677605d4d7b977eda367
takayama <harukigao@gmail.com>
2026-06-25 16:24:44 +0900

    ビルド設定調整

--- Team2 cat/Assets/Developer/Takayama Haruki/Script/GameManger/StartGame.cs
+++ Team2 cat/Assets/Developer/Takayama Haruki/Script/GameManger/StartGame.cs
@@ -1,41 +1,41 @@
-using UnityEngine;
-using Const;
-using System.Collections;
+//using UnityEngine;
+//using Const;
+//using System.Collections;
 
-public class StartGame : MonoBehaviour
-{
-    [SerializeField] GameObject[] set_object; //スタート時にセットするオブジェクト
+//public class StartGame : MonoBehaviour
+//{
+//    [SerializeField] GameObject[] set_object; //スタート時にセットするオブジェクト
 
-    private bool game_start;
-    private float start_time;
-    private float castomer_time; 
+//   // private bool game_start;
+//    private float start_time;
+//    private float castomer_time; 
 
-    public StartGame Instans;
-    void Awake()
-    {
-        Instans = this;
+//    public StartGame Instans;
+//    void Awake()
+//    {
+//        Instans = this;
 
-        game_start = false;
-        for (int i = 0; i < set_object.Length; i++)
-            set_object[i].SetActive(false);
+//        game_start = false;
+//        for (int i = 0; i < set_object.Length; i++)
+//            set_object[i].SetActive(false);
 
-    }
+//    }
 
-    IEnumerator StartMovie()
-    {
-        yield return new WaitForSeconds(start_time);
+//    IEnumerator StartMovie()
+//    {
+//        yield return new WaitForSeconds(start_time);
 
-        //ここで音を鳴らす
+//        //ここで音を鳴らす
 
-        yield return new WaitForSeconds(castomer_time);
+//        yield return new WaitForSeconds(castomer_time);
 
-        //ここでお客さんを登場
+//        //ここでお客さんを登場
 
-        //その次にお客さんのテキスト]
+//        //その次にお客さんのテキスト]
 
-        //if()
+//        //if()
 
-        //ゲーム開始
+//        //ゲーム開始
 
-    }
-}
+//    }
+//}
